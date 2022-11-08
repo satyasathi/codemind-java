c160a38eaabd66d73b4fb62339c882609b0157a6
satyasathi <83865350+satyasathi@users.noreply.github.com>
2022-11-08 21:28:06 +0530

    import java.util.Scanner; class Main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n;         n=sc.nextInt();         int c=0;         for(int i=2;i<=(int)Math.sqrt(n);i++)         {             if(n%i==0)             c++;         }         if(c==0)         System.out.println("prime");         else         System.out.println("not a prime");     } }

---
+++ prime_number.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n;
+        n=sc.nextInt();
+        int c=0;
+        for(int i=2;i<=(int)Math.sqrt(n);i++)
+        {
+            if(n%i==0)
+            c++;
+        }
+        if(c==0)
+        System.out.println("prime");
+        else
+        System.out.println("not a prime");
+    }
+}
